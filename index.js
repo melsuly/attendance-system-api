19f59505c015b58f71e1cc33cd5b2f8d778ea343
Nurasyl Melsuly <nurasylmelsuly@gmail.com>
2024-05-22 23:12:47 +0500

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const bodyParser = require("body-parser")
+const app = express()
+const Routes = require("./routes/route.js")
+
+const PORT = process.env.PORT || 8080
+
+dotenv.config();
+
+// app.use(bodyParser.json({ limit: '10mb', extended: true }))
+// app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
+
+app.use(express.json({ limit: '10mb' }))
+app.use(cors())
+
+mongoose
+    .connect("mongodb+srv://admin:KRZefTld7yjSw2P6@attendancesystem.guq8pgi.mongodb.net/?retryWrites=true&w=majority&appName=AttendanceSystem", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+
+app.use('/', Routes);
+
+app.listen(PORT, () => {
+    console.log(`Server started at port no. ${PORT}`)
+})
